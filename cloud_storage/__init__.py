0f23b0ce60745c389339a3dda090fb44c7b5029b
Gatsby Lee <thewayofthinking@gmail.com>
2019-06-18 10:05:07 -0700

    relocate version info

--- cloud_storage/__init__.py
+++ cloud_storage/__init__.py
@@ -1,8 +1,26 @@
+from cloud_storage.__about__ import __version__
+from cloud_storage.gcs_storage import GoogleCloudStorage
+
+def create_storage_client(name):
+    """
+    Return new CloudStorage client
+
+    Args:
+        name
+    """
+    return GoogleCloudStorage()
+
 def int_or_str(value):
     try:
         return int(value)
     except ValueError:
         return value
 
-__version__ = '1.2.0'
 VERSION = tuple(map(int_or_str, __version__.split('.')))
+
+__all__ = (
+    '__version__',
+    'VERSION',
+    'create_storage_client',
+    'GoogleCloudStorage',
+)
--- cloud_storage/client.py
+++
@@ -1,9 +0,0 @@
-from cloud_storage.gcs_storage import GoogleCloudStorage
-
-def create_storage_client(name):
-    return GoogleCloudStorage()
-
-__all__ = (
-    'create_storage_client',
-    'GoogleCloudStorage',
-)
