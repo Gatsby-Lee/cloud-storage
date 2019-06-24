f77927ef8cdfa0b703cf7b3644aebac6a4f82905
Gatsby Lee <thewayofthinking@gmail.com>
2019-06-24 15:05:00 -0700

    sync download-gzipped functionality

--- README.rst
+++ README.rst
@@ -26,3 +26,59 @@ How to use
 
     >>> from cloud_storage import create_storage_client
     >>> storage = create_storage_client()
+
+
+Playground Test - GCS
+---------------------
+
+.. code-block:: python
+
+    python playground/gcs_storage_playground.py upload --bucket-name <your_bucket> \
+        --object-key "test_sample.html" \
+        --upload-str "<html>hello</html>" \
+        --content-encoding="gzip" \
+        --content-type="text/html"
+
+    python playground/gcs_storage_playground.py download-gzipped --bucket-name <your_bucket>
+        --object-key "test_sample.html"
+
+    python playground/gcs_storage_playground.py download-gzipped --bucket-name <your_bucket>
+        --object-key "test_sample.html" \
+        --do-gunzip
+
+    python playground/gcs_storage_playground.py download-gzipped --bucket-name <your_bucket>
+        --object-key "test_sample.html" \
+        --download-path='test_sample.html'
+
+    python playground/gcs_storage_playground.py download-gzipped --bucket-name <your_bucket>
+        --object-key "test_sample.html" \
+        --do-gunzip \
+        --download-path test_sample.html
+
+
+Playground Test - S3
+---------------------
+
+.. code-block:: python
+
+    python playground/s3_storage_boto3_playground.py upload --bucket-name <your_bucket> \
+        --object-key "test_sample.html" \
+        --upload-str "<html>hello</html>" \
+        --content-encoding="gzip" \
+        --content-type="text/html"
+
+    python playground/s3_storage_boto3_playground.py download-gzipped --bucket-name <your_bucket>
+        --object-key "test_sample.html"
+
+    python playground/s3_storage_boto3_playground.py download-gzipped --bucket-name <your_bucket>
+        --object-key "test_sample.html" \
+        --do-gunzip
+
+    python playground/s3_storage_boto3_playground.py download-gzipped --bucket-name <your_bucket>
+        --object-key "test_sample.html" \
+        --download-path='test_sample.html'
+
+    python playground/s3_storage_boto3_playground.py download-gzipped --bucket-name <your_bucket>
+        --object-key "test_sample.html" \
+        --do-gunzip \
+        --download-path test_sample.html
--- playground/s3_storage_boto3_playground.py
+++ playground/s3_storage_boto3_playground.py
@@ -22,28 +22,25 @@ def _parse_args():
 
     cmd_parser = parser.add_subparsers(dest='cmd')
     cmd_parser.required = True
+
     bucket_cmd_parser = cmd_parser.add_parser('bucket')
     bucket_cmd_subparser = bucket_cmd_parser.add_subparsers(dest='sub_cmd')
     bucket_cmd_subparser.required = True
     bucket_cmd_subparser.add_parser('list')
-
-    download_gzipped_parser = cmd_parser.add_parser(
-        'download-gzipped', parents=[base_parser])
-    download_gzipped_parser.add_argument(
-        '--decode-gzip', action='store_true', default=False)
-
-    download_to_file_parser = cmd_parser.add_parser(
-        'download-to-file', parents=[base_parser])
-    download_to_file_parser.add_argument(
-        '--download-path', required=True)
 
     upload_parser = cmd_parser.add_parser('upload', parents=[base_parser])
     upload_parser.add_argument('--upload-str')
     upload_parser.add_argument('--content-encoding')
     upload_parser.add_argument('--content-type')
 
-    cmd_parser.add_parser('delete', parents=[base_parser])
+    download_gzipped_parser = cmd_parser.add_parser(
+        'download-gzipped', parents=[base_parser])
+    download_gzipped_parser.add_argument(
+        '--do-gunzip', action='store_true')
+    download_gzipped_parser.add_argument(
+        '--download-path')
 
+    cmd_parser.add_parser('delete', parents=[base_parser])
     cmd_parser.add_parser('exists', parents=[base_parser])
 
     return parser.parse_args()
@@ -57,25 +54,19 @@ def _main():
     if args.cmd == 'bucket' and args.sub_cmd == 'list':
         print(client.list_bucket_names())
     elif args.cmd == 'download-gzipped':
-        content = client.download_gzipped(
-            args.bucket_name, args.object_key, args.decode_gzip)
-        if args.decode_gzip:
-            LOGGER.info('Decoding bytes to str.')
-            content = content.decode()
-        print(content)
-    elif args.cmd == 'download-to-file':
-        client.download_to_file(
-            args.bucket_name, args.object_key, args.download_path)
+        if args.download_path:
+            client.download_gzipped_to_file(
+                args.bucket_name, args.object_key, args.download_path, args.do_gunzip)
+        else:
+            content = client.download_gzipped(
+                args.bucket_name, args.object_key, args.do_gunzip)
+            print(content)
     elif args.cmd == 'upload':
         upload_str = args.upload_str.encode()
         if args.content_encoding == 'gzip':
             import gzip
-            size_before_compression = len(upload_str)
             upload_str = gzip.compress(upload_str)
-            size_after_compression = len(upload_str)
-            LOGGER.info(
-                'content-encoding=gzip. applying gzip.compress. before:%s, after:%s',
-                size_before_compression, size_after_compression)
+            LOGGER.info('content-encoding=gzip. applying gzip.compress.')
         client.upload(args.bucket_name, args.object_key, upload_str,
                       args.content_type, args.content_encoding)
     elif args.cmd == 'delete':
